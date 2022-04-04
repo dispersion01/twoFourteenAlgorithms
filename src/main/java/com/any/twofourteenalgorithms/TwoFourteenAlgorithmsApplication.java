2f210eb41c90b3a2b297f3bc6c8a8c8f7c8ac698
dispersion <anyta1901@gmail.com>
2022-04-04 10:41:01 +0300

    Initial commit

---
+++ src/main/java/com/any/twofourteenalgorithms/TwoFourteenAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.any.twofourteenalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoFourteenAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwoFourteenAlgorithmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/any/twofourteenalgorithms/TwoFourteenAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.any.twofourteenalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoFourteenAlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
